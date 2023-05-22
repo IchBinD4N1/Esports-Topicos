1d8319ebc3669320adbecced0e34aeca4ca36a0e
Kartoffelpuree <alanruiz0411@gmail.com>
2023-05-22 03:23:05 -0600

    EditParticipantsV2

--- reactfront/src/components/EditParticipant.js
+++ reactfront/src/components/EditParticipant.js
@@ -2,12 +2,19 @@ import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-const endpoint = 'http://localhost:8000/api/participant/';
+const endpoint = 'http://localhost:8000/api/league/';
 
-const EditParticipant = () => {
-  const [league, setLeague] = useState('');
-  const [team, setTeam] = useState('');
+const validateNumbers = (value) => {
+  const regex = /^[0-9]+$/;
+  return regex.test(value);
+};
+
+const EditLeague = () => {
+  const [name, setName] = useState('');
+  const [location, setLocation] = useState('');
   const [error, setError] = useState('');
   const navigate = useNavigate();
   const { id } = useParams();
@@ -15,95 +22,100 @@ const EditParticipant = () => {
   const update = async (e) => {
     e.preventDefault();
 
-    try {
-      // Obtener el token de autenticación desde el almacenamiento (por ejemplo, localStorage)
-      const token = localStorage.getItem('access_token');
+    if (name.trim() === '') {
+      toast.error('Please enter a name.');
+      return;
+    }
 
-      // Verificar que se haya proporcionado un token válido
+    if (location.trim() === '') {
+      toast.error('Please enter a location.');
+      return;
+    }
+    
+    if (!validateNumbers(team)) {
+      toast.error('Player should contain only numbers.');
+      return;
+    }
+
+    try {
+      const token = localStorage.getItem('access_token');
       if (!token) {
         setError('No se ha proporcionado un token válido');
         return;
       }
 
-      // Configurar el encabezado de autorización con el token
       const config = {
         headers: {
           Authorization: `Bearer ${token}`,
         },
       };
 
-      // Realizar la solicitud PUT con el token de autorización
-      await axios.put(`${endpoint}${id}`, { league, team }, config);
+      await axios.put(`${endpoint}${id}`, { name, location }, config);
 
-      // Redirigir al índice
-      navigate('/showParticipants');
+      navigate('/showLeagues');
     } catch (error) {
       console.error(error);
-      setError('Ha ocurrido un error al actualizar el país');
+      setError('Ha ocurrido un error al actualizar la liga');
     }
   };
 
   useEffect(() => {
-    const getParticipantById = async () => {
+    const getLeagueById = async () => {
       try {
-        // Obtener el token de autenticación desde el almacenamiento (por ejemplo, localStorage)
         const token = localStorage.getItem('access_token');
-
-        // Verificar que se haya proporcionado un token válido
         if (!token) {
           setError('No se ha proporcionado un token válido');
           return;
         }
 
-        // Configurar el encabezado de autorización con el token
         const config = {
           headers: {
             Authorization: `Bearer ${token}`,
           },
         };
 
-        // Realizar la solicitud GET con el token de autorización
         const response = await axios.get(`${endpoint}${id}`, config);
-        setLeague(response.data.league);
-        setTeam(response.data.team);
+        setName(response.data.name);
+        setLocation(response.data.location);
       } catch (error) {
         console.error(error);
-        setError('Ha ocurrido un error al obtener el país');
+        setError('Ha ocurrido un error al obtener la liga');
       }
     };
 
-    getParticipantById();
+    getLeagueById();
   }, [id]);
 
   return (
     <div className="container mt-4">
-      <h3 className="mb-4">Edit Participant</h3>
+      <h3 className="mb-4">Editar Liga</h3>
+      <ToastContainer />
       {error && <div className="alert alert-danger">{error}</div>}
       <form onSubmit={update}>
         <div className="mb-3">
-          <label htmlFor="team" className="form-label">League</label>
+          <label htmlFor="name" className="form-label">Nombre</label>
           <input
-            id="league"
+            id="name"
             type="text"
             className="form-control"
-            value={league}
-            onChange={(e) => setLeague(e.target.value)}
+            value={name}
+            onChange={(e) => setName(e.target.value)}
             required
           />
         </div>
         <div className="mb-3">
-          <label htmlFor="team" className="form-label">Team</label>
+          <label htmlFor="location" className="form-label">Ubicación</label>
           <input
-            id="team"
+            id="location"
             type="text"
             className="form-control"
-            value={team}
-            onChange={(e) => setTeam(e.target.value)}
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
             required
           />
         </div>
         <div className="d-flex justify-content-end">
-        <Link to="/showParticipants" className="btn btn-outline-primary">
+          <Link to="/showLeagues" className="btn btn-outline-primary">
             Go Back
           </Link>
           <button type="submit" className="btn btn-primary">Guardar</button>
@@ -113,4 +125,4 @@ const EditParticipant = () => {
   );
 };
 
-export default EditParticipant;
+export default EditLeague;
