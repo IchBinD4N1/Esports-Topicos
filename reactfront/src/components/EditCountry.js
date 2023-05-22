8d309d78d645a37deefa1748cdecf50629ea9ff2
Kartoffelpuree <alanruiz0411@gmail.com>
2023-05-22 03:20:18 -0600

    EditCountryV2

--- reactfront/src/components/EditCountry.js
+++ reactfront/src/components/EditCountry.js
@@ -2,8 +2,15 @@ import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const endpoint = 'http://localhost:8000/api/country/';
+
+const validateLetters = (value) => {
+  const regex = /^[A-Za-z\s]+$/;
+  return regex.test(value);
+};
 
 const EditCountry = () => {
   const [name, setName] = useState('');
@@ -15,27 +22,42 @@ const EditCountry = () => {
   const update = async (e) => {
     e.preventDefault();
 
-    try {
-      // Obtener el token de autenticación desde el almacenamiento (por ejemplo, localStorage)
-      const token = localStorage.getItem('access_token');
+    if (name.trim() === '') {
+      toast.error('Please enter a name.');
+      return;
+    }
 
-      // Verificar que se haya proporcionado un token válido
+    if (!validateLetters(name)) {
+      toast.error('Name should contain only letters.');
+      return;
+    }
+
+    if (demonym.trim() === '') {
+      toast.error('Please enter a demonym.');
+      return;
+    }
+
+    if (!validateLetters(demonym)) {
+      toast.error('Demonym should contain only letters.');
+      return;
+    }
+
+
+    try {
+      const token = localStorage.getItem('access_token');
       if (!token) {
         setError('No se ha proporcionado un token válido');
         return;
       }
 
-      // Configurar el encabezado de autorización con el token
       const config = {
         headers: {
           Authorization: `Bearer ${token}`,
         },
       };
 
-      // Realizar la solicitud PUT con el token de autorización
       await axios.put(`${endpoint}${id}`, { name, demonym }, config);
 
-      // Redirigir al índice
       navigate('/showCountries');
     } catch (error) {
       console.error(error);
@@ -46,23 +68,18 @@ const EditCountry = () => {
   useEffect(() => {
     const getCountryById = async () => {
       try {
-        // Obtener el token de autenticación desde el almacenamiento (por ejemplo, localStorage)
         const token = localStorage.getItem('access_token');
-
-        // Verificar que se haya proporcionado un token válido
         if (!token) {
           setError('No se ha proporcionado un token válido');
           return;
         }
 
-        // Configurar el encabezado de autorización con el token
         const config = {
           headers: {
             Authorization: `Bearer ${token}`,
           },
         };
 
-        // Realizar la solicitud GET con el token de autorización
         const response = await axios.get(`${endpoint}${id}`, config);
         setName(response.data.name);
         setDemonym(response.data.demonym);
@@ -78,6 +95,7 @@ const EditCountry = () => {
   return (
     <div className="container mt-4">
       <h3 className="mb-4">Editar País</h3>
+      <ToastContainer />
       {error && <div className="alert alert-danger">{error}</div>}
       <form onSubmit={update}>
         <div className="mb-3">
@@ -103,7 +121,7 @@ const EditCountry = () => {
           />
         </div>
         <div className="d-flex justify-content-end">
-        <Link to="/showCountries" className="btn btn-outline-primary">
+          <Link to="/showCountries" className="btn btn-outline-primary">
             Go Back
           </Link>
           <button type="submit" className="btn btn-primary">Guardar</button>
