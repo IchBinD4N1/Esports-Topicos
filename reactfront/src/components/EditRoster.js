57fe2bf38b9bd4d2116cd970893086153cdf6a40
Kartoffelpuree <alanruiz0411@gmail.com>
2023-05-22 03:36:24 -0600

    EditRosterV2

--- reactfront/src/components/EditRoster.js
+++ reactfront/src/components/EditRoster.js
@@ -2,8 +2,15 @@ import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const endpoint = 'http://localhost:8000/api/roster/';
+
+const validateNumbers = (value) => {
+  const regex = /^[0-9]+$/;
+  return regex.test(value);
+};
 
 const EditRoster = () => {
   const [player, setPlayer] = useState('');
@@ -15,60 +22,69 @@ const EditRoster = () => {
   const update = async (e) => {
     e.preventDefault();
 
-    try {
-      // Obtener el token de autenticación desde el almacenamiento (por ejemplo, localStorage)
-      const token = localStorage.getItem('access_token');
+    if (player.trim() === '') {
+      toast.error('Please enter a player.');
+      return;
+    }
 
-      // Verificar que se haya proporcionado un token válido
+    if (!validateNumbers(player)) {
+      toast.error('Player should contain only numbers.');
+      return;
+    }
+
+    if (team.trim() === '') {
+      toast.error('Please enter a team.');
+      return;
+    }
+
+    if (!validateNumbers(team)) {
+      toast.error('Team should contain only numbers.');
+      return;
+    }
+
+    try {
+      const token = localStorage.getItem('access_token');
       if (!token) {
         setError('No se ha proporcionado un token válido');
         return;
       }
 
-      // Configurar el encabezado de autorización con el token
       const config = {
         headers: {
           Authorization: `Bearer ${token}`,
         },
       };
 
-      // Realizar la solicitud PUT con el token de autorización
       await axios.put(`${endpoint}${id}`, { player, team }, config);
 
-      // Redirigir al índice
       navigate('/showRosters');
     } catch (error) {
       console.error(error);
-      setError('Ha ocurrido un error al actualizar el país');
+      setError('Ha ocurrido un error al actualizar el roster');
     }
   };
 
   useEffect(() => {
     const getRosterById = async () => {
       try {
-        // Obtener el token de autenticación desde el almacenamiento (por ejemplo, localStorage)
         const token = localStorage.getItem('access_token');
-
-        // Verificar que se haya proporcionado un token válido
         if (!token) {
           setError('No se ha proporcionado un token válido');
           return;
         }
 
-        // Configurar el encabezado de autorización con el token
         const config = {
           headers: {
             Authorization: `Bearer ${token}`,
           },
         };
 
-        // Realizar la solicitud GET con el token de autorización
         const response = await axios.get(`${endpoint}${id}`, config);
         setPlayer(response.data.player);
         setTeam(response.data.team);
       } catch (error) {
         console.error(error);
-        setError('Ha ocurrido un error al obtener el país');
+        setError('Ha ocurrido un error al obtener el roster');
       }
     };
 
@@ -77,11 +93,12 @@ const EditRoster = () => {
 
   return (
     <div className="container mt-4">
-      <h3 className="mb-4">Edit Roster</h3>
+      <h3 className="mb-4">Editar Roster</h3>
+      <ToastContainer />
       {error && <div className="alert alert-danger">{error}</div>}
       <form onSubmit={update}>
         <div className="mb-3">
-          <label htmlFor="team" className="form-label">Player</label>
+          <label htmlFor="player" className="form-label">Jugador</label>
           <input
             id="player"
             type="text"
@@ -92,7 +109,7 @@ const EditRoster = () => {
           />
         </div>
         <div className="mb-3">
-          <label htmlFor="team" className="form-label">Team</label>
+          <label htmlFor="team" className="form-label">Equipo</label>
           <input
             id="team"
             type="text"
@@ -103,8 +120,8 @@ const EditRoster = () => {
           />
         </div>
         <div className="d-flex justify-content-end">
-        <Link to="/showRosters" className="btn btn-outline-primary">
-            Go Back
+          <Link to="/showRosters" className="btn btn-outline-primary">
+            Regresar
           </Link>
           <button type="submit" className="btn btn-primary">Guardar</button>
         </div>
