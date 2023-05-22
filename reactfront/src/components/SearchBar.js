7b58b00ae016aa96707f168081ab2e8851a25ee7
Kartoffelpuree <alanruiz0411@gmail.com>
2023-05-22 04:49:41 -0600

    SearchBarNew

--- reactfront/src/components/SearchBar.js
+++ reactfront/src/components/SearchBar.js
@@ -8,13 +8,21 @@ const SearchBar = ({ onSearch }) => {
   };
 
   return (
-    <div>
-      <input
-        type="text"
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-      />
-      <button onClick={handleSearch}>Buscar</button>
+    <div className="row">
+      <div className="col-md-6 col-lg-12">
+        <div className="input-group input-group-sm mb-3">
+          <input
+            type="text"
+            className="form-control form-control-sm"
+            placeholder="Search..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <button className="btn btn-primary btn-sm" type="button" onClick={handleSearch}>
+            Search
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
