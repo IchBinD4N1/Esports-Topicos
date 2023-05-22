8afbb05297b3a86f4e91e96b616f1ab8176b3273
Kartoffelpuree <alanruiz0411@gmail.com>
2023-05-22 02:52:14 -0600

    CreateCountryV2

--- reactfront/src/components/CreateCountry.js
+++ reactfront/src/components/CreateCountry.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const endpoint = 'http://localhost:8000/api/country';
 
@@ -12,6 +14,17 @@ const CreateCountry = () => {
 
   const store = async (e) => {
     e.preventDefault();
+
+    if (name.trim() === '') {
+      toast.error('Please enter a name.');
+      return;
+    }
+
+    if (demonym.trim() === '') {
+      toast.error('Please enter a demonym.');
+      return;
+    }
+
     const token = localStorage.getItem('access_token');
     try {
       await axios.post(endpoint, { name, demonym }, { headers: { Authorization: `Bearer ${token}` } });
@@ -22,12 +35,16 @@ const CreateCountry = () => {
   };
 
   return (
-    <div>
+    <div className="container">
       <h3>Create Country</h3>
+      <ToastContainer />
       <form onSubmit={store}>
         <div className="mb-3">
-          <label className="form-label">Name</label>
+          <label htmlFor="name" className="form-label">
+            Name
+          </label>
           <input
+            id="name"
             value={name}
             onChange={(e) => setName(e.target.value)}
             type="text"
@@ -35,20 +52,25 @@ const CreateCountry = () => {
           />
         </div>
         <div className="mb-3">
-          <label className="form-label">Demonym</label>
+          <label htmlFor="demonym" className="form-label">
+            Demonym
+          </label>
           <input
+            id="demonym"
             value={demonym}
             onChange={(e) => setDemonym(e.target.value)}
             type="text"
             className="form-control"
           />
         </div>
-        <Link to="/showCountries" className="btn btn-outline-primary">
-          Go Back
-        </Link>
-        <button type="submit" className="btn btn-primary">
-          Save
-        </button>
+        <div className="d-flex justify-content-between">
+          <Link to="/showCountries" className="btn btn-outline-primary">
+            Go Back
+          </Link>
+          <button type="submit" className="btn btn-primary">
+            Save
+          </button>
+        </div>
       </form>
     </div>
   );
