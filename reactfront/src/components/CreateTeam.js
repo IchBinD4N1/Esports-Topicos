e3a7d3d00193c85d1dfb547929ea42b4c11f2707
Kartoffelpuree <alanruiz0411@gmail.com>
2023-05-22 03:15:47 -0600

    CreateLocationV2

--- reactfront/src/components/CreateTeam.js
+++ reactfront/src/components/CreateTeam.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const endpoint = 'http://localhost:8000/api/team';
 
@@ -10,8 +12,29 @@ const CreateTeam = () => {
   const [location, setLocation] = useState('');
   const navigate = useNavigate();
 
+  const validateNumbers = (value) => {
+    const regex = /^[0-9]+$/;
+    return regex.test(value);
+  };
+
   const store = async (e) => {
     e.preventDefault();
+
+    if (name.trim() === '') {
+      toast.error('Please enter a name.');
+      return;
+    }
+
+    if (location.trim() === '') {
+      toast.error('Please enter a location.');
+      return;
+    }
+
+    if (!validateNumbers(location)) {
+      toast.error('Player should contain only numbers.');
+      return;
+    }
+
     const token = localStorage.getItem('access_token');
     try {
       await axios.post(endpoint, { name, location }, { headers: { Authorization: `Bearer ${token}` } });
@@ -22,8 +45,9 @@ const CreateTeam = () => {
   };
 
   return (
-    <div>
+    <div className="container">
       <h3>Create Team</h3>
+      <ToastContainer />
       <form onSubmit={store}>
         <div className="mb-3">
           <label className="form-label">Name</label>
@@ -43,12 +67,14 @@ const CreateTeam = () => {
             className="form-control"
           />
         </div>
-        <Link to="/showTeams" className="btn btn-outline-primary">
+        <div className="d-flex justify-content-between">
+          <Link to="/showTeams" className="btn btn-outline-primary">
             Go Back
           </Link>
-        <button type="submit" className="btn btn-primary">
-          Save
-        </button>
+          <button type="submit" className="btn btn-primary">
+            Save
+          </button>
+        </div>
       </form>
     </div>
   );
